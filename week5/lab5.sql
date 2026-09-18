b6bbf5a0f967fbb0f482f8ef0db243f8c48015d0
illefeliz72 <elifelize.herreria@lorma.edu>
2026-09-19 04:12:46 +0800

    week5

--- week5/lab5.sql
+++ week5/lab5.sql
@@ -1,23 +1,15 @@
+USE infoman1_vetclinic;
+
 SELECT * FROM pet;
 
 SELECT name, species FROM pet;
 
-SELECT name, species 
-FROM pet 
-WHERE species = 'Dog';
+SELECT name, species FROM pet WHERE species = 'Dog';
 
-SELECT * 
-FROM pet 
-WHERE birth_date > '2020-01-01';
+SELECT name, species, birth_date FROM pet WHERE birth_date > '2020-01-01';
 
-SELECT * 
-FROM appointment 
-WHERE appointment_date >= '2026-09-01';
+SELECT * FROM appointment WHERE appointment_date >= '2026-09-01';
 
-SELECT name, species, birth_date 
-FROM pet 
-WHERE species = 'Cat';
+SELECT appointment_id, appointment_date FROM appointment WHERE appointment_date = '2026-09-01';
 
-SELECT name, species, birth_date 
-FROM pet 
-WHERE species = 'Catt';
+SELECT appointment_id, appointment_date FROM appointment WHERE appointment_date = '2026/09/01';
